b0c128ac602deba9cda60f4a4f0d9cb64e7af68a
persontechnology <persontechnologys@gmail.com>
2021-09-16 16:36:30 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Models\Geo;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//routes para geo
+Route::get(
+    'datos-geo/{codDispositivo}/{codMovistar}/{codClaro}/{codCnt}/{potenciaMovistar}/{potenciaClaro}/{potenciaCnt}/{tiempoActualizacion}'
+    , function (
+        $codDispositivo,
+        $codMovistar,
+        $codClaro,
+        $codCnt,
+        $potenciaMovistar,
+        $potenciaClaro,
+        $potenciaCnt,
+        $tiempoActualizacion
+    ) {
+        $data = array(
+            'codDispositivo'=>$codDispositivo,
+            'codMovistar'=>$codMovistar,
+            'codClaro'=>$codClaro,
+            'codCnt'=>$codCnt,
+            'potenciaMovistar'=>$potenciaMovistar,
+            'potenciaClaro'=>$potenciaClaro,
+            'potemciaCnt'=>$potenciaCnt,
+            'tiempoActualizacion'=>$tiempoActualizacion,
+        );
+
+        $geo=Geo::find(1);
+        if(!$geo){
+            $geo=new Geo();
+        }
+        $geo->codDispositivo=$codDispositivo??'-';
+        $geo->codMovistar=$codMovistar??'-';
+        $geo->codClaro=$codClaro??'-';
+        $geo->codCnt=$codCnt??'-';
+        $geo->potenciaMovistar=$potenciaMovistar??'-';
+        $geo->potenciaClaro=$potenciaClaro??'-';
+        $geo->potemciaCnt=$potenciaCnt??'-';
+        $geo->tiempoActualizacion=$tiempoActualizacion??'-';
+        $geo->save();
+});
+Route::get('/geo-get',function(){
+    return Geo::find(1);
+});
