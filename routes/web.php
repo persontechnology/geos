9826ddf4fe603bceea80b1d557a9637698d8f353
persontechnology <persontechnologys@gmail.com>
2021-09-16 16:42:39 -0500

    first commit 3

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Models\Geo;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('crudbooster:install');
+    return 'ok';
+    // Artisan::call('storage:link');
+    // Artisan::call('key:generate');
+    // Artisan::call('migrate:fresh --seed');
+});
 Route::get('/', function () {
     return view('welcome');
 });
