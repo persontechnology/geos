95cc8b89bf5cfb991abd56c0fc46cad40bc20f87
persontechnology <persontechnologys@gmail.com>
2021-09-23 18:20:31 -0500

    fallossxxda

--- public/webcodecamjs/js/webcodecamjquery.js
+++ public/webcodecamjs/js/webcodecamjquery.js
@@ -58,7 +58,7 @@
             flipHorizontal: false,
             zoom: 0,
             beep: '',
-            decoderWorker: '../js/DecoderWorker.js',
+            decoderWorker: '../../webcodecamjs/js/DecoderWorker.js',
             brightness: 0,
             autoBrightnessValue: 0,
             grayScale: 0,
